428d44b677c0e8027dc493e653a268ef322855db
Ruan <ruans@fivefriday.com>
2022-05-03 00:27:48 +0200

    Ruan Test Commit
    I made changes as I felt needed. This improved responsiveness, validation and also display of information on the front-end.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
-    <input type="number" v-model="limit" />
-    <Numbers/>
+      <!-- 
+      - As requested under instruction values should be between 0 - 100 and not more,
+      - Therefore I added additional validation to insure values stay between 0 - 100
+      -->
+      <label for="numInput" id="labelIn">Instert a Value between 0 - 100</label> <br/>
+        <input type="number" v-model="limit" placeholder=" Enter number between 0-100"/>
+          <div class="error" v-if="limit > 100"> Incorrect Input - Please insert correct number between 1 - 100</div>
+      <!-- 
+      - It is really important to me to always remove clutter from the front-end and therefore I decided to implement a "if"
+        statement where if the users input is bigger than 100 it will display an error message and remove all numbers.
+        The thought behind this was to block a user from typing infinite numbers into the input box and display all the numbers
+        in an infinite list/loop.
+      -->
+      <div v-if="limit <= 100">
+        <Numbers/>
+      </div>
+      <div v-else="limit > 100"></div>
   </div>
 </template>
 
@@ -14,11 +29,58 @@ export default {
   data()
   {
     return {
-      limit: 100
+      limit: 100, 
     }
-  }
+  },
+
 }
+
 </script>
 
 <style scoped>
+
+/* I added additional styling to create a more structured display */
+
+#app{
+  background:#D3D3D3;
+  opacity: 1;
+  margin: 0 auto;
+  width: 700px;
+  max-height: fit-content;
+  border: 2px solid;
+  border-radius: 3px;
+  text-align: center;
+  padding: 15px;
+}
+
+/* Adding of a error display is always a create way to improve responsiveness. Therefore I added in an error message */
+
+.error{
+  position: relative;
+  font-weight: 600;
+  border: 1px solid;
+  border-radius: 5px;
+  padding: 15px;
+  width: 26.6em;
+  background: red;
+  opacity: 0.3;
+  color: #fff;
+  margin:0 auto;
+  margin-top: 15px;
+}
+
+input[type="number"]{
+  margin-top: 15px;
+  width: 240px;
+  padding: 15px; 
+  text-align: center;
+}
+
+#labelIn{
+  font-weight: 600;
+  font-size: 20px;
+  margin-bottom: 15px;
+  margin-top: 15px;
+}
+
 </style>
--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
+  <div class="container">
+    <!-- I believe the best way for tracking mouse movements would rather be to use vue.js "v-on" functions like shown below -->
+    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" v-on:mouseover="hov(number)" v-on:mouseout="reset">
       {{number}}
-    </div>
+    </div>   
   </div>
 </template>
 
@@ -12,7 +13,7 @@ export default {
   {
     return {
       limit: this.$parent.limit,
-      numbers: []
+      numbers: [],
     }
   },
   watch: {
@@ -30,11 +31,11 @@ export default {
         numbers = [...numbers, i];
       }
       return numbers.sort(() => Math.random() - 0.5);
+      
     },
     hov(number)
     {
       const nums = document.querySelectorAll('.number');
-
       for(let i = 0; i < nums.length; i++)
       {
         const num = nums[i].textContent.trim();
@@ -48,21 +49,49 @@ export default {
     reset()
     {
       const nums = document.querySelectorAll('.number');
-      nums.forEach(num => num.classList.remove('active'))
-    }
+      nums.forEach(num => num.classList.remove('active')) 
+    },
   }
 }
 </script>
 
 <style scoped>
+
 	.number {
 		display: inline-block;
 		padding: 5px;
 		background-color: lightgrey;
 		margin: 5px;
+    width: 2em;
+    text-align: center;
+    border-radius: 2px;
 	}
+
+  /* 
+  I was thinking of creating a method/function where you just send over the divirsor and display them on the front-end, I rather chose
+  to just display the divisors in red and the active number in blue to display the difference better.
+  */
+
+  .number:hover{
+    background-color:blue;
+  }
 
 	.active {
 		background-color: red;
+    opacity: 0.3;
+    color: #FFF;
 	}
+
+  /* 
+  As most of the logical functioning works perfectly I thought that the main focus should be to implement front-end changes and make the 
+  array of numbers easier to read and display. Therefore I create another div/container around all the elements and styled them accordingly.
+  */
+
+  .container{
+    margin: auto;
+    max-width: fit-content;
+    max-height: fit-content;
+    padding: 10px;
+  }
+  
 </style>
